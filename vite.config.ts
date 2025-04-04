f9a263995426ba506db61c14defce35d60a81bb9
Pierre Mesure <pierre@mesu.re>
2025-04-04 17:24:05 +0200

    Upgrade dependencies and remove vite-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { VitePWA } from "vite-plugin-pwa";
 import Icons from "unplugin-icons/vite";
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    VitePWA(),
     Icons({
       compiler: "jsx",
       jsx: "react",
